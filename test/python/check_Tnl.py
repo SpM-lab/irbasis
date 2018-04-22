8a1c86a48ee362fa12c5b8447662404c65449f34
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2018-04-22 23:59:13 +0900

    update test for limit tnl

--- test/python/check_Tnl.py
+++ test/python/check_Tnl.py
@@ -34,12 +34,25 @@ class refdata(object):
             self._Tnl_ref_max = f[prefix+'/data/Tnl_max'].value
 
     def check_data(self, basis, prefix):
-            rf = ir.basis("../irbasis.h5", prefix)
-            ndim = rf.dim() if rf.dim()%2 ==0 else rf.dim()-1
-            Tnl = rf.compute_Tnl(self._Tnl_ref[:, 0])[:, ndim-1]
+            ndim = basis.dim() if basis.dim()%2 ==0 else basis.dim()-1
+            Tnl = basis.compute_Tnl(self._Tnl_ref[:, 0])[:, ndim-1]
             dTnl = abs(Tnl- self._Tnl_ref[:,1])/self._Tnl_ref_max
-            return numpy.max(dTnl)
             
+            nTnl = [ _data[0]*_data[1].imag for _data in self._Tnl_ref]
+            Tnl_limit_imag = (basis.ulx(ndim-1, 1)+basis.ulx(ndim-1, -1))/(numpy.pi*math.sqrt(2.0))
+            dnTnl_imag = abs(Tnl_limit_imag-nTnl[len(nTnl)-1]) \
+                    if abs(Tnl_limit_imag) < math.pow(10.0, -12.0) \
+                    else abs(Tnl_limit_imag-nTnl[len(nTnl)-1])/Tnl_limit_imag
+            dTnl = numpy.append(dTnl, dnTnl_imag)
+
+            nnTnl = [ -_data[0]*_data[0]*_data[1].real for _data in self._Tnl_ref]
+            Tnl_limit_real = (basis.d_ulx(ndim-1, 1, 1)+basis.d_ulx(ndim-1, -1, 1))/(numpy.pi*numpy.pi*math.sqrt(2.0))
+            dnnTnl_real = abs(Tnl_limit_real-nnTnl[len(nnTnl)-1]) \
+                    if abs(Tnl_limit_real) < math.pow(10.0, -12.0) \
+                    else abs(Tnl_limit_real-nnTnl[len(nnTnl)-1])/Tnl_limit_real
+            dTnl = numpy.append(dTnl, dnnTnl_real)            
+            return numpy.max(dTnl)
+
 class TestMethods(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         
@@ -52,8 +65,6 @@ class TestMethods(unittest.TestCase):
                 rf_ref = refdata("../tnl_safe_ref.h5", prefix)            
                 basis = ir.basis("../irbasis.h5", prefix)
                 self.assertLessEqual(rf_ref.check_data(basis, prefix), math.pow(10.0, -8))
-
-
 
 if __name__ == '__main__':
     unittest.main()
