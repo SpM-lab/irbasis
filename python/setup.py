12768a9f1889a259e819e3eb5e8aac2a0675cd43
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2018-05-02 21:24:00 +0900

    Implemented setup.py

---
+++ python/setup.py
@@ -0,0 +1,64 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+#with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    #long_description = f.read()
+
+
+setup(
+    name='irbasis',
+
+    version='0.1',
+
+    description='Python libraries for irbasis',
+
+    #long_description=long_description,  # Optional
+
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/SpM-lab/irbasis',
+
+    author='Kazuyoshi Yoshimi, Hiroshi Shinaoka, Chikano Naoya, Junya Otsuki',
+
+    author_email='h.shinaoka@gmail.com', 
+
+    classifiers=[ 
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='quantum mony-body theory',
+
+    packages = find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    install_requires=['numpy', 'scipy', 'h5py'],
+
+    package_data={
+        'irbasis': ['irbasis.h5'],
+    },
+)
