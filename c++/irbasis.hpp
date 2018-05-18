d21317c678f73b1051d8a7fe0067bbcb39ab3836
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2018-05-18 09:13:53 +0900

    add reading info data

--- c++/irbasis.hpp
+++ c++/irbasis.hpp
@@ -14,179 +14,205 @@
 
 namespace {
 
-    namespace internal {
+  namespace internal {
 
-        // Simple implementation without meta programming...
-        template<typename T, int DIM>
-        class multi_array {
-            public:
-                multi_array() : data_(0) {
-                }
+    // Simple implementation without meta programming...
+    template<typename T, int DIM>
+    class multi_array {
+    public:
+      multi_array() : data_(0) {
+      }
 
-                multi_array(int N1) : data_(N1) {
-                    assert(DIM == 1);
-                    extents_[0] = N1;
-                }
+      multi_array(int N1) : data_(N1) {
+        assert(DIM == 1);
+        extents_[0] = N1;
+      }
 
-                multi_array(int N1, int N2) : data_(N1*N2) {
-                    assert(DIM == 2);
-                    extents_[0] = N1;
-                    extents_[1] = N2;
-                }
+      multi_array(int N1, int N2) : data_(N1 * N2) {
+        assert(DIM == 2);
+        extents_[0] = N1;
+        extents_[1] = N2;
+      }
 
-                multi_array(int N1, int N2, int N3) : data_(N1*N2*N3) {
-                    assert(DIM == 3);
-                    extents_[0] = N1;
-                    extents_[1] = N2;
-                    extents_[2] = N3;
-                }
+      multi_array(int N1, int N2, int N3) : data_(N1 * N2 * N3) {
+        assert(DIM == 3);
+        extents_[0] = N1;
+        extents_[1] = N2;
+        extents_[2] = N3;
+      }
 
-                multi_array(std::size_t* dims) {
-                    resize(dims);
-                }
+      multi_array(std::size_t *dims) {
+        resize(dims);
+      }
 
-                std::size_t extent(int i) const {
-                    assert(i >= 0);
-                    assert(i < DIM);
-                    return extents_[i];
-                }
+      std::size_t extent(int i) const {
+        assert(i >= 0);
+        assert(i < DIM);
+        return extents_[i];
+      }
 
-                void resize(std::size_t* dims) {
-                    std::size_t tot_size = std::accumulate(dims, dims + DIM, 1, std::multiplies<std::size_t>());
-                    data_.resize(tot_size);
-                    for (int i=0; i<DIM; ++i) {
-                        extents_[i] = dims[i];
-                    }
-                }
-
-                std::size_t num_elements() const {
-                    return data_.size();
-                }
-
-                T* origin() {
-                    return &data_[0];
-                }
-
-                T& operator()(int i) {
-                    assert (DIM == 1);
-                    int idx = i;
-                    assert(idx >= 0 && idx < data_.size());
-                    return data_[idx];
-                }
-
-                const T& operator()(int i) const {
-                    assert (DIM == 1);
-                    int idx = i;
-                    assert(idx >= 0 && idx < data_.size());
-                    return data_[idx];
-                }
-
-                T& operator()(int i, int j) {
-                    assert (DIM == 2);
-                    int idx = extents_[1] * i + j;
-                    assert(idx >= 0 && idx < data_.size());
-                    return data_[idx];
-                }
-
-                const T& operator()(int i, int j) const {
-                    assert (DIM == 2);
-                    int idx = extents_[1] * i + j;
-                    assert(idx >= 0 && idx < data_.size());
-                    return data_[idx];
-                }
-
-                T& operator()(int i, int j, int k) {
-                    assert (DIM == 3);
-                    int idx = (i * extents_[1] +  j) * extents_[2] + k;
-                    assert(idx >= 0 && idx < data_.size());
-                    return data_[idx];
-                }
-
-                const T& operator()(int i, int j, int k) const {
-                    assert (DIM == 3);
-                    int idx = (i * extents_[1] +  j) * extents_[2] + k;
-                    assert(idx >= 0 && idx < data_.size());
-                    return data_[idx];
-                }
-
-            private:
-                std::vector<T> data_;
-                std::size_t extents_[DIM];
-        };
-        
-        // https://www.physics.ohio-state.edu/~wilkins/computing/HDF/hdf5tutorial/examples/C/h5_rdwt.c
-        // https://support.hdfgroup.org/ftp/HDF5/current/src/unpacked/examples/h5_read.c
-        // read a double
-        inline double hdf5_read_double(hid_t& file, const std::string& name) {
-            hid_t dataset = H5Dopen2(file, name.c_str(), H5P_DEFAULT);
-            double data;
-            H5Dread(dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &data);
-            H5Dclose(dataset);
-            return data;
+      void resize(std::size_t *dims) {
+        std::size_t tot_size = std::accumulate(dims, dims + DIM, 1, std::multiplies<std::size_t>());
+        data_.resize(tot_size);
+        for (int i = 0; i < DIM; ++i) {
+          extents_[i] = dims[i];
         }
+      }
 
-        // read an int
-        inline int hdf5_read_int(hid_t& file, const std::string& name) {
-            hid_t dataset = H5Dopen2(file, name.c_str(), H5P_DEFAULT);
-            int data;
-            H5Dread(dataset, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &data);
-            H5Dclose(dataset);
-            return data;
-        }
-        
-        // read array of double
-        template<int DIM>
-        void hdf5_read_double_array(hid_t& file, const std::string& name, std::vector<std::size_t>& extents, std::vector<double>& data) {
-            hid_t dataset = H5Dopen2(file, name.c_str(), H5P_DEFAULT);
-            hid_t space = H5Dget_space(dataset);
-            std::vector<hsize_t> dims(DIM);
-            int n_dims = H5Sget_simple_extent_dims(space, &dims[0], NULL);
-            assert(n_dims == DIM);
-            std::size_t tot_size = std::accumulate(dims.begin(), dims.end(), 1, std::multiplies<double>());
-            data.resize(tot_size);
-            extents.resize(DIM);
-            for (int i=0; i<DIM; ++i) {
-                extents[i] = static_cast<std::size_t>(dims[i]);
-            }
-            H5Dread(dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &data[0]);
-            H5Dclose(dataset);
-        }
-        
-        // read double multi_array
-        template<int DIM>
-        multi_array<double,DIM> load_multi_array(hid_t& file, const std::string& name) {
-            hid_t dataset = H5Dopen2(file, name.c_str(), H5P_DEFAULT);
-            hid_t space = H5Dget_space(dataset);
-            std::vector<hsize_t> dims(DIM);
-            int n_dims = H5Sget_simple_extent_dims(space, &dims[0], NULL);
-            assert(n_dims == DIM);
-            std::size_t tot_size = std::accumulate(dims.begin(), dims.end(), 1, std::multiplies<std::size_t>());
-            std::vector<std::size_t> extents(DIM);
-            for (int i=0; i<DIM; ++i) {
-                extents[i] = static_cast<std::size_t>(dims[i]);
-            }
-            multi_array<double,DIM> a;
-            a.resize(&extents[0]);
-            H5Dread(dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, a.origin());
-            std::vector<double> data(tot_size);
-            H5Dread(dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &data[0]);
-            multi_array<double,DIM> b = a;
-            H5Dclose(dataset);
+      std::size_t num_elements() const {
+        return data_.size();
+      }
 
-            return a;
-        }
+      T *origin() {
+        return &data_[0];
+      }
+
+      T &operator()(int i) {
+        assert(DIM == 1);
+        int idx = i;
+        assert(idx >= 0 && idx < data_.size());
+        return data_[idx];
+      }
+
+      const T &operator()(int i) const {
+        assert(DIM == 1);
+        int idx = i;
+        assert(idx >= 0 && idx < data_.size());
+        return data_[idx];
+      }
+
+      T &operator()(int i, int j) {
+        assert(DIM == 2);
+        int idx = extents_[1] * i + j;
+        assert(idx >= 0 && idx < data_.size());
+        return data_[idx];
+      }
+
+      const T &operator()(int i, int j) const {
+        assert(DIM == 2);
+        int idx = extents_[1] * i + j;
+        assert(idx >= 0 && idx < data_.size());
+        return data_[idx];
+      }
+
+      T &operator()(int i, int j, int k) {
+        assert(DIM == 3);
+        int idx = (i * extents_[1] + j) * extents_[2] + k;
+        assert(idx >= 0 && idx < data_.size());
+        return data_[idx];
+      }
+
+      const T &operator()(int i, int j, int k) const {
+        assert(DIM == 3);
+        int idx = (i * extents_[1] + j) * extents_[2] + k;
+        assert(idx >= 0 && idx < data_.size());
+        return data_[idx];
+      }
+
+    private:
+      std::vector <T> data_;
+      std::size_t extents_[DIM];
+    };
+
+    // https://www.physics.ohio-state.edu/~wilkins/computing/HDF/hdf5tutorial/examples/C/h5_rdwt.c
+    // https://support.hdfgroup.org/ftp/HDF5/current/src/unpacked/examples/h5_read.c
+    // read a double
+    inline double hdf5_read_double(hid_t &file, const std::string &name) {
+      hid_t dataset = H5Dopen2(file, name.c_str(), H5P_DEFAULT);
+      double data;
+      H5Dread(dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &data);
+      H5Dclose(dataset);
+      return data;
     }
 
-    /*
-    class basis {
-    public:
-        basis(
-            const std::string& file_name,
-            const std::string& prefix = "",
-        ) throw(std::runtime_error) {
-        }
+    // read an int
+    inline int hdf5_read_int(hid_t &file, const std::string &name) {
+      hid_t dataset = H5Dopen2(file, name.c_str(), H5P_DEFAULT);
+      int data;
+      H5Dread(dataset, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &data);
+      H5Dclose(dataset);
+      return data;
+    }
 
-    };
-    */
+    // read array of double
+    template<int DIM>
+    void hdf5_read_double_array(hid_t &file, const std::string &name, std::vector <std::size_t> &extents,
+                                std::vector<double> &data) {
+      hid_t dataset = H5Dopen2(file, name.c_str(), H5P_DEFAULT);
+      hid_t space = H5Dget_space(dataset);
+      std::vector <hsize_t> dims(DIM);
+      int n_dims = H5Sget_simple_extent_dims(space, &dims[0], NULL);
+      assert(n_dims == DIM);
+      std::size_t tot_size = std::accumulate(dims.begin(), dims.end(), 1, std::multiplies<double>());
+      data.resize(tot_size);
+      extents.resize(DIM);
+      for (int i = 0; i < DIM; ++i) {
+        extents[i] = static_cast<std::size_t>(dims[i]);
+      }
+      H5Dread(dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &data[0]);
+      H5Dclose(dataset);
+    }
+
+    // read double multi_array
+    template<int DIM>
+    multi_array<double, DIM> load_multi_array(hid_t &file, const std::string &name) {
+      hid_t dataset = H5Dopen2(file, name.c_str(), H5P_DEFAULT);
+      hid_t space = H5Dget_space(dataset);
+      std::vector <hsize_t> dims(DIM);
+      int n_dims = H5Sget_simple_extent_dims(space, &dims[0], NULL);
+      assert(n_dims == DIM);
+      std::size_t tot_size = std::accumulate(dims.begin(), dims.end(), 1, std::multiplies<std::size_t>());
+      std::vector <std::size_t> extents(DIM);
+      for (int i = 0; i < DIM; ++i) {
+        extents[i] = static_cast<std::size_t>(dims[i]);
+      }
+      multi_array<double, DIM> a;
+      a.resize(&extents[0]);
+      H5Dread(dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, a.origin());
+      std::vector<double> data(tot_size);
+      H5Dread(dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, &data[0]);
+      multi_array<double, DIM> b = a;
+      H5Dclose(dataset);
+
+      return a;
+    }
+  }
+
+
+  class basis {
+  public:
+    basis(
+            const std::string &file_name,
+            const std::string &prefix = ""
+    ) throw(std::runtime_error) {
+      hid_t file = H5Fopen(file_name.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
+      lambda_ = internal::hdf5_read_double(file, prefix + std::string("/info/Lambda"));
+      dim_ = internal::hdf5_read_int(file, prefix + std::string("/info/dim"));
+      statistics_ = f[prefix + '/info/statistics'].value == 0 ? "B" : "F";
+      std::vector <std::size_t> extents;
+      internal::hdf5_read_double_array<1>(file, prefix + std::string("/sl"), extents, sl_);
+
+      H5Fclose(file);
+    }
+
+    /**
+      * Return number of basis functions
+      * @return  number of basis functions
+      */
+    int dim() const { return dim_; }
+
+    double sl(int l) const throw(std::runtime_error) {
+      assert(l >= 0 && l < dim());
+      return static_cast<double>(sl_[l]);
+    }
+
+  private:
+    double Lambda_;
+    int dim_;
+    std::string statistics_;
+    std::vector<double> sl_;
+
+  };
+
 
 }
