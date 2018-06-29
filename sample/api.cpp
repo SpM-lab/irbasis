dae00f058f3f5892c2bfcf0a021d81cd6c584926
Naoya Chikano <n.chikano.005@ms.saitama-u.ac.jp>
2018-06-29 12:57:39 +0900

    debug

---
+++ sample/api.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include "irbasis.hpp"
+
+int main() {
+	double Lamb = 1000.0;
+	irbasis::basis b = irbasis::load("F", Lamb, "./irbasis.h5");
+	
+	//Dimensions of basis
+	std::cout << b.dim() << std::endl;
+	
+	//u_l(x = 1) and v_l(y = 1)
+	std::cout << b.ulx(0,1) << std::endl;
+	std::cout << b.vly(0,1) << std::endl;
+	
+	//Singular value s_0
+	std::cout << b.sl(0) << std::endl;
+	
+	//The k-th derivative of ulx and vly
+	std::cout << b.d_ulx(0,1,1) << std::endl;
+	std::cout << b.d_vly(0,1,1) << std::endl;
+	
+	//Fourier taransform of ulx
+	//std::vector<std::vector<std::complex<double>>> vec{0};
+	std::vector<double> vec{0};
+	std::cout << b.compute_Tnl(vec)[0][0] << std::endl;
+	
+}
