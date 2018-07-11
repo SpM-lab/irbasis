ee37119bac2bc020027d941010d8ba086211b3d1
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2018-07-12 08:47:32 +0900

    Reformatted api.py

--- sample/api.py
+++ sample/api.py
@@ -2,13 +2,14 @@ from __future__ import print_function
 import numpy
 import irbasis
 
+# By default, Lambda = 10, 100, 1000, 10000 are available.
 Lambda = 1000.0
 
 # Fermionic
 # If you has not installed the irbasis package via pip,
 # you must specify the location of a data file as follows.
 #   irbasis.load('F',  Lambda, './irbasis.h5')
-basis = irbasis.load('F',  Lambda)
+basis = irbasis.load('F', Lambda)
 
 l = 0
 print("l =",l,",Lambda =", Lambda)
@@ -17,20 +18,20 @@ x = 1
 y = 1
 
 # Dimensions of basis
-print("Dim ",basis.dim())
+print("Dim ", basis.dim())
 
 # u_0(x = 1) and v_0(y = 1) 
-print("ulx ",basis.ulx(l,x))
-print("vly ",basis.vly(l,y))
+print("ulx ", basis.ulx(l,x))
+print("vly ", basis.vly(l,y))
 
 # Singular value s_0
-print("sl ",basis.sl(l))
+print("sl ", basis.sl(l))
 
 # The k-th derivative of u_l(x) and v_l(y)  (k = 1,2,3)
-for k in [1,2,3]:
-    print("k = ",k)
-    print("d_ulx ",basis.d_ulx(l,x,k))
-    print("d_vly ",basis.d_vly(l,y,k))
+for k in [1, 2, 3]:
+    print("k = ", k)
+    print("d_ulx ", basis.d_ulx(l,x,k))
+    print("d_vly ", basis.d_vly(l,y,k))
 
 # Fourier transform of ulx
 n = numpy.arange(1000)
