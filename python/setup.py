89c9b5f5092c5f4b35f446c17973eee4ece74bc1
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2019-06-07 22:45:25 +0900

    Version 1.0.4: data up to Lambda=10^7 included

--- python/setup.py
+++ python/setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='irbasis',
 
-    version='1.0.3',
+    version='1.0.4',
 
     description='Python libraries for irbasis',
 
